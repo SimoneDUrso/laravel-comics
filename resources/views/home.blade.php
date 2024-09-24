d9adbc536cebfcebb657b7ba906ce295eb0df886
SimoneDUrso <simone.durso98@gmail.com>
2024-09-24 17:37:33 +0200

    [UPD] Added blue section and footer section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,10 +31,100 @@
 
     <div class="container-full bg-primary blue-section">
         <div class="row justify-content-center align-items-center">
-            <div class="col-2" v-for="nav, index in navMain">
+            <div class="col-2 d-flex">
                 <div class="d-flex justify-content-center align-items-center">
-                    <img src="nav.image" alt="Immagini">
-                    <span class="ms-2"><a class="text-decoration-none text-white"> PROVA </a></span>
+                    <img src=" {{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="Immagini">
+                    <span class="ms-2"><a class="text-decoration-none text-white"> DIGITAL COMICS </a></span>
+                </div>
+            </div>
+            <div class="col-2 d-flex">
+                <div class="d-flex justify-content-center align-items-center">
+                    <img src=" {{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="Immagini">
+                    <span class="ms-2"><a class="text-decoration-none text-white"> DC MERCHANDISE </a></span>
+                </div>
+            </div>
+            <div class="col-2 d-flex">
+                <div class="d-flex justify-content-center align-items-center">
+                    <img src=" {{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="Immagini">
+                    <span class="ms-2"><a class="text-decoration-none text-white"> SUBSCRIPTIONS </a></span>
+                </div>
+            </div>
+            <div class="col-2 d-flex">
+                <div class="d-flex justify-content-center align-items-center">
+                    <img src=" {{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="">
+                    <span class="ms-2"><a class="text-decoration-none text-white"> COMICS SHOP LOCATOR </a></span>
+                </div>
+            </div>
+            <div class="col-2 d-flex">
+                <div class="d-flex justify-content-center align-items-center">
+                    <img src=" {{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="">
+                    <span class="ms-2"><a class="text-decoration-none text-white"> DC POWER VISA </a></span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="bg-image bg-dark">
+
+        <div class="container pt-5">
+            <div class="row">
+                <div class="col-12 d-flex p-0">
+                    <div class="d-flex">
+                        <div class="me-4">
+                            <h4 class="text-white">DC COMICS</h4>
+                            <ul class="list-unstyled">
+                                <li><a class="text-decoration-none" href="#">Characters</a></li>
+                                <li><a class="text-decoration-none" href="#">Comics</a></li>
+                                <li><a class="text-decoration-none" href="#">Movies</a></li>
+                                <li><a class="text-decoration-none" href="#">TV</a></li>
+                                <li><a class="text-decoration-none" href="#">Games</a></li>
+                                <li><a class="text-decoration-none" href="#">Videos</a></li>
+                                <li><a class="text-decoration-none" href="#">News</a></li>
+                            </ul>
+
+                            <h4 class="text-white">SHOP</h4>
+                            <ul class="list-unstyled">
+                                <li><a class="text-decoration-none" href="#">Shop DC</a></li>
+                                <li><a class="text-decoration-none" href="#">Shop DC Collectibles</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="mx-4">
+                            <h4 class="text-white">DC</h4>
+
+                            <ul class="list-unstyled">
+                                <li><a class="text-decoration-none" href="#">Terms Of Use</a></li>
+                                <li><a class="text-decoration-none" href="#">Privacy policy (New)</a></li>
+                                <li><a class="text-decoration-none" href="#">Ad Choices</a></li>
+                                <li><a class="text-decoration-none" href="#">Advertising</a></li>
+                                <li><a class="text-decoration-none" href="#">Jobs</a></li>
+                                <li><a class="text-decoration-none" href="#">Subscriptions</a></li>
+                                <li><a class="text-decoration-none" href="#">Talent Workshops</a></li>
+                                <li><a class="text-decoration-none" href="#">CPSC Certificates</a></li>
+                                <li><a class="text-decoration-none" href="#">Ratings</a></li>
+                                <li><a class="text-decoration-none" href="#">Shop Help</a></li>
+                                <li><a class="text-decoration-none" href="#">Contact Us</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="mx-4">
+                            <h4 class="text-white">SITES</h4>
+                            <ul class="list-unstyled">
+                                <li><a class="text-decoration-none" href="#">DC</a></li>
+                                <li><a class="text-decoration-none" href="#">MAD Magazine</a></li>
+                                <li><a class="text-decoration-none" href="#">DC Kids</a></li>
+                                <li><a class="text-decoration-none" href="#">DC Universe</a></li>
+                                <li><a class="text-decoration-none" href="#">DC Power Visa</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="dc-logo-bg position-relative">
+                            <img class="position-absolute img-fluid"
+                                src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="">
+                        </div>
+                    </div>
+
+
                 </div>
             </div>
         </div>
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,15 @@
 <footer>
-
+    <footer class="position-relative">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 ps-0 mt-5 d-flex justify-content-between align-items-center fs-5">
+                    <button class="btn btn-lg border border-primary">SIGN-UP NOW!</button>
+                    <div>
+                        <span class="text-primary mx-2 fw-bold">FOLLOW US</span>
+                        <span class="mx-2"><a href="#"><img src="icon.image" alt=""></a></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 </footer>
